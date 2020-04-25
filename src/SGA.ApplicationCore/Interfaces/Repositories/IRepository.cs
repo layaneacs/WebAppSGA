3cc9f0dbf7bbcd41e05dc32b0c90b2d6b97ed96f
layane.andreia@hotmail.com <layane.andreia@hotmail.com>
2020-04-24 23:18:47 -0300

    Add IRepository na Interface e Repository na Infra

---
+++ src/SGA.ApplicationCore/Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace SGA.ApplicationCore.Interfaces.Repositories
+{
+    public interface IRepository<TEntity> where TEntity : class // restrição do tipo generico...* TEntity.. você que escolhe
+    {/* Ler mais sobre interfaces genericas */
+        TEntity Adicionar(TEntity entity);
+        void Atualizar(TEntity entity);
+        IEnumerable<TEntity> ObterTodos();
+        TEntity ObterPorId(int id);
+        IEnumerable<TEntity> Buscar(Expression<Func<TEntity, bool>> predicado);
+        void Remover(TEntity entity);
+    }
+}
